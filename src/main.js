26efc859df531a93145c531df608c62484ceb9c1
Yaroslav <craazzy777@gmail.com>
2025-02-28 13:05:31 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var vue_1 = require("vue");
-var pinia_1 = require("pinia");
-var App_vue_1 = require("./App.vue");
-var app = (0, vue_1.createApp)(App_vue_1.default);
-app.use((0, pinia_1.createPinia)());
-app.mount('#app');
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(createPinia())
+app.mount('#app')
